d986f0be3ffa3d2e0949cc071d9200f2a705736b
Caleb R. Redus <credus@pacbell.net>
2011-01-12 15:54:05 -0800

    removed get commented lines from code as these lines are no longer necessary

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,6 @@ SampleApp::Application.routes.draw do
 
   root :to => 'pages#home'
 
-  #get "pages/home"
-
-  #get "pages/contact"
-
-  #get "pages/about"
-
-  #get "pages/help"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
