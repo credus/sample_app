15003d30b9f73bb10abeff57a638c3fc64377efb
Caleb R. Redus <credus@pacbell.net>
2011-01-12 17:57:52 -0800

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up" )
+  end
 
   it "should have a Home page at '/'" do
     get '/'
